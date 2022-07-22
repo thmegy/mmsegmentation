0005aa7320b10ea74a46a1ada2a7bc30e4b27f4c
Théo Megy <theo.megy@logiroad.fr>
2022-07-22 14:32:33 +0200

    update cracks dataset

--- mmseg/datasets/cracks.py
+++ mmseg/datasets/cracks.py
@@ -9,22 +9,22 @@ class CracksDataset(CustomDataset):
     """Cracks dataset.
 
     In segmentation map annotation , 0 stands for background,
-    which is not included in 6 categories. ``reduce_zero_label`` is fixed to
-    True. The ``img_suffix`` is fixed to '.jpg' and ``seg_map_suffix`` is
+    which is included in 7 categories. ``reduce_zero_label`` is fixed to
+    False. The ``img_suffix`` is fixed to '.jpg' and ``seg_map_suffix`` is
     fixed to '.png'.
 
     """
 
-    CLASSES = ('Arrachement_pelade', 'Faiencage', 'Nid_de_poule',
+    CLASSES = ('Background', 'Arrachement_pelade', 'Faiencage', 'Nid_de_poule',
                  'Transversale', 'Longitudinale', 'Reparation')
 
-    PALETTE = [[120, 120, 120], [180, 120, 120], [6, 230, 230], [80, 50, 50],
-               [4, 200, 3], [120, 120, 80]]
+    PALETTE = [[120, 120, 120], [220, 10, 50], [6, 230, 230], [20, 10, 10],
+               [220, 130, 30], [240, 240, 70], [60, 220, 30]]
 
     def __init__(self, **kwargs):
         super(CracksDataset, self).__init__(
             img_suffix='.jpg',
             seg_map_suffix='.png',
-            reduce_zero_label=True,
+            reduce_zero_label=False,
             **kwargs)
         
